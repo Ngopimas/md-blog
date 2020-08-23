d0571afd7d05e051e69ec9f8bd9ade2dff73c19c
Romain Coupey <schaft115@hotmail.com>
2020-08-23 18:23:39 +0200

    create & show

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false}))
 
-app.get('/', (req, res) => {
-  const articles = [{
-    title: 'test',
-    createdAt: Date.now(),
-    description: 'this is a test'
-  }]
-  res.render('index', {articles: articles})
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: 'desc'
+  })
+  res.render('articles/index', {articles: articles})
 });
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)

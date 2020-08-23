136319ba6d00b26f1fdac68fa36aed55e9543d5b
Romain Coupey <schaft115@hotmail.com>
2020-08-23 16:14:00 +0200

    start home front

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  const articles = [{
+    title: 'test',
+    createdAt: Date.now(),
+    description: 'this is a test'
+  }]
+  res.render('index', {articles: articles})
 });
 
 app.listen(5000)
